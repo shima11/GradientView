c887288df1037ea19021e3d210f22f09715c51cb
shima <shima11@users.noreply.github.com>
2016-11-14 22:28:39 +0900

    gradient view 作成

--- GradientView/GradientView.swift
+++ GradientView/GradientView.swift
@@ -9,13 +9,36 @@
 import UIKit
 
 class GradientView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+ 
+    init(view: UIView) {
+        super.init(frame: view.frame)
+        setup(view: view)
     }
-    */
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setup(view: UIView) {
+        let topColor = UIColor.white // 開始色
+        let bottomColor = UIColor.white.withAlphaComponent(0.0) // 終了色
+        let gradientColors: [CGColor] = [topColor.cgColor, bottomColor.cgColor]
+        
+        //グラデーションレイヤーを作成
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
+        gradientLayer.frame = CGRect(x:0, y:0, width:view.frame.width, height:view.frame.height)
+        
+        // グラデーションの方向（横）
+        gradientLayer.startPoint = CGPoint(x:0, y:0.5);
+        gradientLayer.endPoint = CGPoint(x:1.0, y:0.5);
+        
+        //グラデーションの色をレイヤーに割り当てる
+        gradientLayer.colors = gradientColors
+        
+        gradientLayer.locations = [0.1, 0.6]
+        
+        //グラデーションレイヤーをビューに追加        
+        view.layer.addSublayer(gradientLayer)
+    }
+
 }
--- GradientView/ViewController.swift
+++ GradientView/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let gradientView = GradientView(view: imageView)
+        self.view.addSubview(gradientView)
+        
+        
+        print(imageView.frame)
+        print(gradientView.frame)
+        
+//        gradientView.backgroundColor = UIColor.red
+        
     }
 
     override func didReceiveMemoryWarning() {
