e29218d27c641dcf92d21298eee80ca79a1bbdd0
shima <shima11@users.noreply.github.com>
2016-11-14 21:47:32 +0900

    initial commit

---
+++ GradientView/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  GradientView
+//
+//  Created by shima jinsei on 2016/11/14.
+//  Copyright © 2016年 Jinsei Shima. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+}
